a435412a3a973548bf22c9c55f19edc14204127f
alakhpandya <alakhpandya@gmail.com>
2022-12-15 18:04:12 +0530

    dec15 create

--- javascript/projects/modal/main.js
+++ javascript/projects/modal/main.js
@@ -5,3 +5,7 @@ const exit_btn = document.querySelector('.exit-btn')
 btn.addEventListener('click', ()=>{
     chat_container.classList.add('active');
 })
+
+exit_btn.addEventListener('click', ()=>{
+    chat_container.classList.remove('active');
+})
